9affb8c83584d2a3772c04918708fdbff94ed828
Maxheath1 <meeks6907@yahoo.com>
2024-02-11 15:42:29 -0700

    moved everything from develop file and it works now lol

--- models/index.js
+++ models/index.js
@@ -1,30 +1,35 @@
-const Category = require('./Category');
 const Product = require('./Product');
+const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Associations
+
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-});
+  foreignKey: "category_id",
+}),
 
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-});
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+})
 
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id',
-});
+  through: {
+    model:ProductTag,
+    unique: false,
+  },
+})
 
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id',
-});
+  through:{
+    model:ProductTag,
+    unique: false,
+  },
+})
 
 module.exports = {
-  Category,
   Product,
+  Category,
   Tag,
   ProductTag,
 };
